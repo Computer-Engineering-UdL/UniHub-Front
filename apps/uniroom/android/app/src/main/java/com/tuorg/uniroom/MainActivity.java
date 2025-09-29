f9b1c5fc7f9d2bdb47273e6e425a86d543d6a536
Aniol0012 <aniol0012@gmail.com>
2025-09-29 16:55:13 +0200

    init: remove angular and add ionic project

---
+++ apps/uniroom/android/app/src/main/java/com/tuorg/uniroom/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tuorg.uniroom;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/uniroom/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuorg.uniroom',
+  appName: 'uniroom',
+  webDir: 'www'
+};
+
+export default config;
