e5b1dddb4e4ca050a582001d1c8644281e5de3eb
Aniol0012 <aniol0012@gmail.com>
2025-09-29 17:03:34 +0200

    feat: add prettier as dependency

--- apps/uniroom/capacitor.config.ts
+++ apps/uniroom/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tuorg.uniroom',
   appName: 'uniroom',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
