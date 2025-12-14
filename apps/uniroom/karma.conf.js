7c91124607f44be0045f611fb8df1655c6b3226e
Aniol0012 <aniol0012@gmail.com>
2025-12-14 16:56:15 +0100

    fix: sonarqube

--- apps/uniroom/karma.conf.js
+++ apps/uniroom/karma.conf.js
@@ -25,9 +25,13 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly', file: 'lcov.info' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
