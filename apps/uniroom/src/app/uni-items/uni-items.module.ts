cb9c04359c8c4b9ef2bd34b953ecddf12b98ad30
Aniol0012 <aniol0012@gmail.com>
2025-12-14 21:28:54 +0100

    feat: enhance object detail page

--- apps/uniroom/src/app/uni-items/uni-items.module.ts
+++ apps/uniroom/src/app/uni-items/uni-items.module.ts
@@ -7,10 +7,19 @@ import { UniItemsRoutingModule } from './uni-items-routing.module';
 import { ItemsListPage } from './items-list/items-list.page';
 import { ItemDetailPage } from './item-detail/item-detail.page';
 import { ItemEditPage } from './item-edit/item-edit.page';
+import { SharedModule } from '../shared/shared-module';
 
 @NgModule({
   declarations: [ItemsListPage, ItemDetailPage, ItemEditPage],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, TranslateModule, UniItemsRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    TranslateModule,
+    UniItemsRoutingModule,
+    SharedModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UniItemsModule {}
