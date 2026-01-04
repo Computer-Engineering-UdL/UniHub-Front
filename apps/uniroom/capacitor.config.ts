405af8ce8722c4ede17277905753279037926ca7
Aniol0012 <aniol0012@gmail.com>
2026-01-04 20:52:11 +0100

    fix: android build

--- apps/uniroom/capacitor.config.ts
+++ apps/uniroom/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tuorg.uniroom',
   appName: 'UniHub',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    allowNavigation: ['api.unihub.smuks.dev']
+  }
 };
 
 export default config;
